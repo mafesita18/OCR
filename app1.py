b9e9136cd17965194cb7d9d7f9d0d2b10786e6a4
mafesita18 <109915580+mafesita18@users.noreply.github.com>
2026-09-08 17:11:42 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -10,17 +10,13 @@ with st.sidebar:
     filtro = st.radio("Aplicar Filtro", ('Con Filtro', 'Sin Filtro'))
 
 if img_file_buffer is not None:
-    # Cargar la imagen directamente con PIL
     image = Image.open(img_file_buffer)
     
-    # Aplicar filtro de inversión si se selecciona
     if filtro == 'Con Filtro':
-        # Convierte a RGB si es necesario y luego invierte colores
         image_to_process = ImageOps.invert(image.convert('RGB'))
     else:
         image_to_process = image
 
-    # Aplicar OCR directamente sobre la imagen de PIL
     text = pytesseract.image_to_string(image_to_process)
     
     st.write("### Texto detectado:")
