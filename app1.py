30de501809ddc1a3d89d9bd096fcc05dc652de2f
mafesita18 <109915580+mafesita18@users.noreply.github.com>
2026-09-08 17:06:18 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,34 +1,30 @@
 import streamlit as st
-import cv2
-import numpy as np
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageOps
 
-
-st.title("Reconocimiento óptico de Caracteres")
+st.title("Reconocimiento Óptico de Caracteres")
 
 img_file_buffer = st.camera_input("Toma una Foto")
 
 with st.sidebar:
-      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
-
+    filtro = st.radio("Aplicar Filtro", ('Con Filtro', 'Sin Filtro'))
 
 if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    # Cargar la imagen directamente con PIL
+    image = Image.open(img_file_buffer)
     
+    # Aplicar filtro de inversión si se selecciona
     if filtro == 'Con Filtro':
-         cv2_img=cv2.bitwise_not(cv2_img)
+        # Convierte a RGB si es necesario y luego invierte colores
+        image_to_process = ImageOps.invert(image.convert('RGB'))
     else:
-         cv2_img= cv2_img
-    
-        
-    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text=pytesseract.image_to_string(img_rgb)
-    st.write(text) 
-    
+        image_to_process = image
 
+    # Aplicar OCR directamente sobre la imagen de PIL
+    text = pytesseract.image_to_string(image_to_process)
+    
+    st.write("### Texto detectado:")
+    st.write(text)
 
     
 
